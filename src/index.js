4dcccc804c7ddca8761c977fc1ce5a50e3ac2858
Christopher Schuster <cs@livoris.de>
2016-01-19 15:35:06 -0800

    fixes change propagation for signals

--- src/index.js
+++ src/index.js
@@ -1,37 +1,62 @@
 import sweet from 'sweet.js';
 
-const macros = `
+// could use webpacke file loader but that does not work for mocha
+let macros = `
 macro rlet {
   case {
-    _ $varname:ident = initially ($init:expr) subscribe($deps:ident (,) ... ) = $expr:expr
+    _ $varname:ident = subscribe($deps:ident (,) ... ) initially ($init:expr) $expr:expr
   } => {
     var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
     letstx $s = [makeIdent('__' + randLetter + Date.now() % 10000, #{$varname})];
-    randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    letstx $var = [makeIdent('__' + randLetter + (1 + Date.now() % 10000), #{$varname})];
-    letstx $dots = [makePunc('...', #{$varname})];
     return #{
-      let $s = new Signal(function() {
-        $( $deps read ; ) ... return $expr;
-      });
       let $varname = macro {
         rule { = $next:expr } => {
           $s.push($next)
         }
-        rule { read ; $rest $dots } => {
-          macro $var {
-            rule { } => { $s.read() }
-          }
-          $rest $dots
-        }
         rule { subscribe $update:ident ; } => {
           $s.onUpdate($update);
         }
-        rule { } => { $var }
+        rule { } => { $s.read() }
       }
-      let update = function() { $s.update(); };
-      $( $deps subscribe update ; ) ...
+      let $s = new Signal(function() { return $expr; }, $init);
+      $( $deps subscribe $s ; ) ...
     }
+  }
+
+  case {
+    _ $varname:ident = subscribe($sub:ident (,) ... , $sube:expr $rest ... ) initially($init:expr) $expr:expr
+  } => {
+    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    letstx $tmp = [makeIdent('__' + randLetter + Date.now() % 10000, #{$varname})];
+    return #{
+      rlet $tmp;
+      ($sube)(function(w) { $tmp = w; })
+      rlet $varname = subscribe($sub (,) ... , $tmp $rest ...) initially($init) $expr
+    };
+  }
+
+  case {
+    _ $varname:ident = subscribe($sube:expr $rest ... ) initially($init:expr) $expr:expr
+  } => {
+    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    letstx $tmp = [makeIdent('__' + randLetter + Date.now() % 10000, #{$varname})];
+    return #{
+      rlet $tmp;
+      ($sube)(function(w) { $tmp = w; })
+      rlet $varname = subscribe($tmp $rest ...) initially($init) $expr
+    };
+  }
+
+  rule {
+    $varname:ident = initially($init ...) $expr:expr
+  } => {
+    rlet $varname = subscribe() initially($init ...) $expr
+  }
+
+  rule {
+    $varname:ident = subscribe($sub ...) $expr:expr
+  } => {
+    rlet $varname = subscribe($sub ...) initially(null) $expr
   }
 
   rule {
@@ -41,28 +66,31 @@ macro rlet {
   }
 
   rule {
-    $varname:ident = initially($init ...) $expr:expr
+    $varname:ident
   } => {
-    rlet $varname = initially($init ...) subscribe() $expr
+    rlet $varname = null
   }
+}
 
+macro subscribe {
   rule {
-    $varname:ident = subscribe($sub ...) $expr:expr
+    ( $deps ... ) { $stmt ... }
   } => {
-    rlet $varname = initially(null) subscribe($sub ...) $expr
+    rlet x = subscribe ( $deps ... ) (function() { $stmt ... })()
   }
-}`;
+}
+`;
 
 class Signal {
-  constructor(expr) {
+  constructor(expr, initial) {
     this.subscribers = [];
     this.expr = expr;
-    this.last = expr();
+    this.last = initial;
   }
 
   push(val) {
     this.last = val;
-    this.subscribers.forEach(s => s());
+    this.subscribers.forEach(s => s.update());
   }
 
   read() {
@@ -71,12 +99,11 @@ class Signal {
 
   onUpdate(subscriber) {
     this.subscribers.push(subscriber);
-    subscriber();
   }
 
   update() {
     this.last = this.expr();
-    this.subscribers.forEach(s => s());
+    this.subscribers.forEach(s => s.update());
   }
 }
 
--- test/simple.js
+++ test/simple.js
@@ -1,44 +1,118 @@
-import rlet from '../index';
+import rlet from '../src/index';
 import {expect} from 'chai';
 
 describe('rlet', () => {
-  it('should support simple values', (done) => {
-    const src = `rlet a = 3; subscribe(a) { global.f(a); }`;
+  it('should support imperative updates', (done) => {
+    const src = `
+      rlet a;
+      global.g = function() { a = true };
+      subscribe(a) { global.f(a); }`;
     global.f = (v) => {
-      expect(v).to.be.equal(3);
+      expect(v).to.be.equal(true);
       done();
     };
     rlet(src);
+    global.g();
   });
 
-  it('should support imperative updates', (done) => {
+  it('should support updates and references', (done) => {
     const src = `
-      rlet a = 3;
-      global.g = function() { a = 5 };
+      rlet a;
+      global.g = function() { a = a ? false : true };
       subscribe(a) { global.f(a); }`;
     let first = true;
     global.f = (v) => {
       if (first) {
-        expect(v).to.be.equal(3);
+        expect(v).to.be.equal(true);
         first = false;
         global.g();
       } else {
-        expect(v).to.be.equal(5);
+        expect(v).to.be.equal(false);
         done();
       }
     };
     rlet(src);
+    global.g();
   });
 
   it('should support reactive updates', (done) => {
     const src = `
-      rlet a = 3;
-      rlet b(a) = a + 1;
+      rlet a;
+      rlet b = subscribe(a) a + 1;
+      global.g = function() { a = 5 };
       subscribe(b) { global.f(b); }`;
     global.f = (v) => {
-      expect(v).to.be.equal(4);
+      expect(v).to.be.equal(6);
       done();
     };
     rlet(src);
+    global.g();
+  });
+
+  it('should set up dependencies automatically', (done) => {
+    const src = `
+      rlet a = 3;
+      rlet b = a + 1;
+      global.g = function() { a = 5 };
+      subscribe(b) { global.f(b); }`;
+    global.f = (v) => {
+      expect(v).to.be.equal(6);
+      done();
+    };
+    rlet(src);
+    global.g();
+  });
+
+  it('should support subscriptions to other reactive variables', (done) => {
+    const src = `
+      rlet a = 3;
+      rlet b = subscribe(a) true;
+      global.g = function() { a = 5 };
+      subscribe(b) { global.f(b); }`;
+    global.f = (v) => {
+      expect(v).to.be.equal(true);
+      done();
+    };
+    rlet(src);
+    global.g();
+  });
+
+  it('should support subscriptions with callbacks', (done) => {
+    const src = `
+      global.f = null;
+      global.g = function(cb) { global.f = cb; }
+      rlet a = subscribe(global.g) true;
+      subscribe(a) { global.h(a); }`;
+    global.h = (v) => {
+      expect(v).to.be.equal(true);
+      done();
+    };
+    rlet(src);
+    global.f();
+  });
+
+  it('should support folding', (done) => {
+    const src = `
+      rlet a;
+      rlet b = subscribe(a) initially(0) b + 1;
+      global.g = function() { a = true; };
+      subscribe(b) { global.f(b); }`;
+    let first = true;
+    global.f = (v) => {
+      if (first) {
+        expect(v).to.be.equal(1);
+        first = false;
+        global.g();
+      } else {
+        expect(v).to.be.equal(2);
+        done();
+      }
+    };
+    rlet(src);
+    global.g();
+  });
+
+  it('prevents glitches', (done) => {
+    done(false);
   });
 });
