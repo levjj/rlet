1deaf9750ba75cf206ab85182140f3ee2e6c192c
Christopher Schuster <cs@livoris.de>
2016-01-27 12:46:52 -0800

    fixed reference error in immutability tests

--- test/immutability.js
+++ test/immutability.js
@@ -4,14 +4,17 @@ import {expect} from 'chai';
 describe('functional rlet evaluation', () => {
   it('should suppress global variable assignments', () => {
     const src = `
+      rlet b;
+      rlet c = subscribe(b) (a = true);`;
+    expect(() => evalR(src)).to.throw(ReferenceError);
+  });
+
+  it('should suppress surrounding variable assignments', () => {
+    const src = `
       var a;
       rlet b;
-      rlet c = subscribe(b) (a = true);
-      window.g = function() { b = true };
-      subscribe(a) { window.f(a); }`;
-    evalR(src);
-    expect(() => window.g())
-      .to.throw(new Error('State updates in reactive variables not allowed'));
+      rlet c = subscribe(b) (a = true);`;
+    expect(() => evalR(src)).to.throw(ReferenceError);
   });
 
   it('should suppress field updates', () => {
@@ -19,10 +22,8 @@ describe('functional rlet evaluation', () => {
       var a = {};
       rlet b;
       rlet c = subscribe(b) (a.prop = true);
-      window.g = function() { b = true };
-      subscribe(a) { window.f(a); }`;
+      window.g = function() { b = true };`;
     evalR(src);
-    expect(() => window.g())
-      .to.throw(new Error('State updates in reactive variables not allowed'));
+    expect(() => window.g()).to.throw(TypeError);
   });
 });
